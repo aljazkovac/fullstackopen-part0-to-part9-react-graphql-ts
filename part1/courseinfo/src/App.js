9ffd6518c71849d751d4b08feef8694946f82b58
Aljaz Kovac <aljazkovac@Aljazs-MacBook-Pro.local>
2022-07-30 11:13:24 +0200

    step 2 finished

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -20,19 +20,24 @@ const App = () => {
 const Header = (props) => {
   return (
     <>
-      <p>
-        {props.course}
-      </p>
+      <p>{props.course}</p>
     </>
   )
 }
 const Content = (props) => {
   return (
   <>
-  <p>{props.part1} {props.exercises1}</p>
-  <p>{props.part2} {props.exercises2}</p>
-  <p>{props.part3} {props.exercises3}</p>
+  <Part part={props.part1} exercises={props.exercises1} />
+  <Part part={props.part2} exercises={props.exercises2} />
+  <Part part={props.part3} exercises={props.exercises3} />
   </>
+  )
+}
+const Part = (props) => {
+  return (
+    <>
+    <p>{props.part} {props.exercises}</p>
+    </>
   )
 }
 const Total = (props) => {
