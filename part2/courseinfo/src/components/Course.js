3fddfd87d138ff10d8bc0975a75e5a9c045bb08d
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-08-09 21:12:21 +0200

    Course info step 7 done: show the total number of exercises.

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Content from "./Content"
+import Total from "./Total"
 
 const Course = (props) => {
     return (
         <>
             <Header courseName={props.course.name} />
             <Content parts={props.course.parts} />
+            <Total courseParts={props.course.parts} />
         </>
     )
 }
--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,7 +1,10 @@
 const Total = (props) => {
+  const total = props.courseParts.reduce(function(sum, part) {
+    return sum + part.exercises
+  }, 0)
   return (
     <>
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <p>Number of exercises: {total}</p>
     </>
   )
 }
