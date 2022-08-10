c85853ceb5da3b44bdcf9ad3540c4b343b88f598
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-08-10 20:31:51 +0200

    Phonebook step 1 done.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,48 @@
 import { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 1 }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personObject))
+    // The following line ensures that the name does not remain
+    // written in the input box after submitting 
+    setNewName('')
+  }
+  const handleNewNote = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input 
+            value={newName}
+            onChange={handleNewNote}
+                />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person => 
+          <Person key={person.id} person={person} />
+        )} 
+      </div>
+      <div>debug: {newName}</div>
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,5 @@
+const Person = ({ person }) => (
+    <p>{person.name}</p>
+)
+
+export default Person
