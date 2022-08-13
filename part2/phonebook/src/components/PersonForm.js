1e20d8090e7c97764fd90198e009cafc252a1991
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-08-13 21:00:28 +0200

    Step 6 phonebook done. The server and the db.json file are set up. A small mistake in the PersonForm is corrected.

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,20 +1,14 @@
 const PersonForm = ( {addPerson, newName, handleNewName,
                      newNumber, handleNewNumber} ) => (
       <form onSubmit={addPerson}>
-        <tr>
-            <td>
             Name: <input 
                 value={newName}
                 onChange={handleNewName} 
                     />
-            </td>
-            <td>
             Number: <input 
                 value={newNumber}
                 onChange={handleNewNumber}
                     />
-            </td>
-        </tr>
         <div>
           <button type="submit">Add</button>
         </div>
