805388ba1fa33a273f12c32b079c141c9eb6fa40
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-09-04 13:23:24 +0200

    Exercise 3.8 completed.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+morgan.token('data', (req) => JSON.stringify(req.body))
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
     { 
@@ -85,7 +90,7 @@ app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(person => person.id !== id)
     res.status(204).end()
   })
-  
+
 const PORT = 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
