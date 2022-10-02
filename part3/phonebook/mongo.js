41b55cfc8aeccd236b4b7c13a2da3aea1359288f
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-10-02 23:26:22 +0200

    Changed how a name is saved (as a string, not an array of strings).

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,36 +1,12 @@
 const mongoose = require('mongoose')
-
-
 const password = process.argv[2]
-
 const url = `mongodb+srv://aljaz:${password}@cluster0.l8aozni.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 const entrySchema = new mongoose.Schema({
-  name: [String],
+  name: String,
   number: Number,
 })
-
 const Entry = mongoose.model('Entry', entrySchema)
 
-if (process.argv.length > 3 && process.argv.length < 5) {
-    console.log("Please provide a phone number!")
-}
-if (process.argv.length >= 5) {
-    mongoose
-        .connect(url)
-        .then((result) => {
-        console.log('connected')
-        const entry = new Entry({
-            name : process.argv.slice(3, -1),
-            number: process.argv.at(-1)
-        })
-        return entry.save()
-        })
-        .then(() => {
-        console.log('Entry saved!')
-        return mongoose.connection.close()
-        })
-        .catch((err) => console.log(err))
-}
 if (process.argv.length === 3) {
     mongoose
         .connect(url)
@@ -41,4 +17,27 @@ if (process.argv.length === 3) {
             })
             mongoose.connection.close()
         })})
+}
+if (process.argv.length === 5) {
+    let entry;
+    mongoose
+        .connect(url)
+        .then((result) => {
+        console.log('connected')
+        entry = new Entry({
+            name : process.argv.at(3),
+            number: process.argv.at(4)
+        })
+        return entry.save()
+        })
+        .then(() => {
+        console.log(`Added ${entry.name} with number 
+        ${entry.number} to the phonebook.`)
+        return mongoose.connection.close()
+        })
+        .catch((err) => console.log(err))
+}
+else if (process.argv.length > 5) {
+    console.log("Too many arguments")
+    console.log("Use like this: node mongo.js password name number")
 }
