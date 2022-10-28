4a4f9945abf00352f5a85a142b7b61e69fc9a472
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-10-28 22:22:29 +0200

    Restructured the project according to instructions.

---
+++ part4/models/entry.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+// eslint-disable-next-line no-undef
+const entrySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      }
+    }
+  }
+})
+entrySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+module.exports = mongoose.model('Entry', entrySchema)
