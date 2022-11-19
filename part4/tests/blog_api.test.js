4cd1d94652649674d5766ac4a3df0de4414d5eaa
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-11-19 11:10:38 +0100

    Implemented some basic tests for the test DB.

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+}, 100000)
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(2)
+}, 100000)
+
+test('the first blog is about React patterns', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].title).toBe('React patterns')
+}, 100000)
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain(
+    'Go To Statement Considered Harmful'
+  )}, 100000)
+
+afterAll(() => {
+  mongoose.connection.close()
+}, 100000)
