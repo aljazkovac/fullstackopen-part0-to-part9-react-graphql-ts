fea1330d662af24016e942ba207fb24edb9f4597
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-11-20 20:57:41 +0100

    Introduced the 'express-async-errors' library.
    Modified the requests i.e., removed the try-catch blocks.

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -6,45 +6,30 @@ blogRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogRouter.get('/:id', async (request, response, next) => {
-  try {
-    const blog = await Blog.findById(request.params.id)
-    if (blog) {
-      response.json(blog)
-    } else {
-      response.status(404).end()
-    }
-  }
-  catch (exception) {
-    next(exception)
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
   }
 })
 
-blogRouter.post('/', async (request, response, next) => {
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
     likes: request.body.likes
   })
-  try {
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-  }
-  catch (exception) {
-    next(exception)
-  }
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
-blogRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Blog
-      .findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  }
-  catch (exception) {
-    next(exception)
-  }
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog
+    .findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogRouter
