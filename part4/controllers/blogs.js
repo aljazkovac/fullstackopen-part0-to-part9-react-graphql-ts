cdcec61da8b8a1c9ad70dabce78c42c8e4c1db0e
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2022-12-04 10:41:23 +0100

    Limit creating new blogs to logged in users. More error handling.

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -16,9 +17,20 @@ blogRouter.get('/:id', async (request, response) => {
     response.status(404).end()
   }
 })
-
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 blogRouter.post('/', async (request, response) => {
-  const user = await User.findById(request.body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
