ec8dca1b92195e73de32e42f01992e994f987dfe
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-01-01 15:13:22 +0100

    Implement 5.1 and some more. Need to do log-out next.

--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -1,9 +1,24 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.post(baseUrl, newObject, config)
+  return request.then(response => response.data)
+}
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
 
-export default { getAll }
+export default { getAll, create, update, setToken }
