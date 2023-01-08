a11e631d17c32b71bf6259f58287fa6f5eb307f3
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-01-08 23:28:54 +0100

    Implement a table just for fun. Will not be used in the continuation of the exercises.

--- part5/src/components/BlogList.js
+++ part5/src/components/BlogList.js
@@ -1,10 +1,87 @@
+import React, {useMemo} from 'react'
+import { useTable } from 'react-table'
 import BlogRow from './BlogRow'
 
 const BlogList = ({blogs}) => {
+    const data = useMemo(
+        () => blogs, [blogs]
+      )
+      const columns = useMemo(
+        () => [
+          {
+            Header: 'Author',
+            accessor: 'author', // accessor is the "key" in the data
+          },
+          {
+            Header: 'Title',
+            accessor: 'title',
+          },
+          {
+            Header: 'Url',
+            accessor: 'url',
+          },
+          {
+            Header: 'Likes',
+            accessor: 'likes',
+          },
+        ],
+        []
+      )
+      const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({ columns, data })
     return(
-        blogs.map(blog =>
-        <BlogRow key={blog.id} blog={blog} />
-        )
+        //blogs.map(blog =>
+        //<BlogRow key={blog.id} blog={blog} />
+        //)
+        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+       <thead>
+         {headerGroups.map(headerGroup => (
+           <tr {...headerGroup.getHeaderGroupProps()}>
+             {headerGroup.headers.map(column => (
+               <th
+                 {...column.getHeaderProps()}
+                 style={{
+                   borderBottom: 'solid 3px red',
+                   background: 'aliceblue',
+                   color: 'black',
+                   fontWeight: 'bold',
+                 }}
+               >
+                 {column.render('Header')}
+               </th>
+             ))}
+           </tr>
+         ))}
+       </thead>
+       <tbody {...getTableBodyProps()}>
+         {rows.map(row => {
+           prepareRow(row)
+           return (
+             <tr {...row.getRowProps()}>
+               {row.cells.map(cell => {
+                 return (
+                   <td
+                     {...cell.getCellProps()}
+                     style={{
+                       padding: '10px',
+                       border: 'solid 1px gray',
+                       background: 'papayawhip',
+                     }}
+                   >
+                     {cell.render('Cell')}
+                   </td>
+                 )
+               })}
+             </tr>
+           )
+         })}
+       </tbody>
+     </table>
     )
 }
 
