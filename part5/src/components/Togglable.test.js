59e9b8a4efba0cb36b3470dd5e4f46ae9a96bc3c
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-01-29 23:11:34 +0100

    Add another test for Togglable component that checks the functioning of the cancel button.

--- part5/src/components/Togglable.test.js
+++ part5/src/components/Togglable.test.js
@@ -40,4 +40,16 @@ describe('<Togglable />', () => {
     const div = container.querySelector('.togglableContent')
     expect(div).not.toHaveStyle('display: none')
   })
+
+  test('toggled content can be closed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const closeButton = screen.getByText('cancel')
+    await user.click(closeButton)
+
+    const div = container.querySelector('.togglableContent')
+    expect(div).toHaveStyle('display: none')
+  })
 })
