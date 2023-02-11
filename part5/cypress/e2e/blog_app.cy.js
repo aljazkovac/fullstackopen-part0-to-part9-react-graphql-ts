7fe4c77fa9dc7597e6c468976fb5e50d1dc3b09a
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-02-11 23:55:02 +0100

    Add testing router to backend. Implement initialization of DB before all tests.

--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -1,7 +1,15 @@
 describe('Blog app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'test user',
+      username: 'tester',
+      password: 'testerpassword'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
+
   describe('login functionality', function() {
     it('front page can be opened', function() {
       cy.contains('The Blogosphere')
@@ -12,17 +20,18 @@ describe('Blog app', function() {
     })
     it('user can login', function() {
       cy.contains('login').click()
-      cy.get('#username-input').type('root')
-      cy.get('#password-input').type('superpassword')
+      cy.get('#username-input').type('tester')
+      cy.get('#password-input').type('testerpassword')
       cy.get('#login-button').click()
-      cy.contains('superuser logged in')
+      cy.contains('test user logged in')
     })
   })
+
   describe('when logged in', function() {
     beforeEach(function() {
       cy.contains('login').click()
-      cy.get('#username-input').type('root')
-      cy.get('#password-input').type('superpassword')
+      cy.get('#username-input').type('tester')
+      cy.get('#password-input').type('testerpassword')
       cy.get('#login-button').click()
     })
     it('a new blog can be created', function() {
