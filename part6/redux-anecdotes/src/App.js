117727dd61902e2591dd5409120eb5546f8ae7b1
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-02-23 18:19:23 +0100

    Initial setup. Implement NewAnecdote component.

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux'
+import NewAnecdote from './components/NewAnecdote'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <NewAnecdote />
+    </div>
+  )
+}
+
+export default App
