0395116a0a0b8f48a3daf40b6c2f311053cf350f
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-04-05 00:35:39 +0200

    added query-anecdotes, setup

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,46 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery(
+    'notes',
+    () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+  )
+  console.log(result)
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
