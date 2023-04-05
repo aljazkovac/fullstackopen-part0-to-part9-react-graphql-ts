17fa7910f69cb6c6cd2f615e1ffad46cef1c362d
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-04-05 23:23:55 +0200

    Implement adding new anecdotes and voting for them using React Query.
    Ex. 6.21-6.22 done.

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,22 +1,36 @@
-import { useQuery } from 'react-query'
-import axios from 'axios'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
 
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: (updatedAnecdote) => {
+      console.log('Updated anecdote', updatedAnecdote)
+      const updatedAnecdotes = anecdotes.map((anecdote) =>
+        anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote
+      );
+      queryClient.setQueryData('anecdotes', updatedAnecdotes);
+    }
+  })
+
   const handleVote = (anecdote) => {
     console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes+1})
   }
 
-  const result = useQuery(
-    'notes',
-    () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
-  )
-  console.log(result)
+  const result = useQuery('anecdotes', getAnecdotes)  
+  console.log('Result', result)
 
   if ( result.isLoading ) {
     return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <span>Error: {result.error.message}</span>
   }
 
   const anecdotes = result.data
--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,16 +1,32 @@
+import { useMutation, useQueryClient } from "react-query"
+import { createAnecdote } from "../requests"
+
 const AnecdoteForm = () => {
 
-  const onCreate = (event) => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onError: (error) => {
+      console.log("Error from useMutation:", error);
+    },
+    onSuccess: (newAnecdote) => {
+      console.log('New anecdote', newAnecdote)
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+    }
+  })
+
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  }
 
   return (
     <div>
       <h3>create new</h3>
-      <form onSubmit={onCreate}>
+      <form onSubmit={addAnecdote}>
         <input name='anecdote' />
         <button type="submit">create</button>
       </form>
