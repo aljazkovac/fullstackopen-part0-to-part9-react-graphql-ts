e0c9f351a4008e6debf027730f6d8b3b6c84c239
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-05-14 21:25:35 +0200

    Setup Prettier.

--- part7/blogs-redux/.eslintrc.js
+++ part7/blogs-redux/.eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
   },
   'extends': [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'prettier',
   ],
   'parserOptions': {
     'ecmaFeatures': {
@@ -21,30 +22,12 @@ module.exports = {
     'react', 'jest', 'cypress'
   ],
   'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
     'quotes': [
       'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
+      'single',
+      { 'avoidEscape': true, 'allowTemplateLiterals': false }
     ],
     'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
     'no-console': 0,
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 'off'
