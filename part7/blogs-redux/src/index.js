bacd6cb31ce8e8bdf6f3db845023ca24b2770a9d
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-05-14 20:00:28 +0200

    Initialize project. Add store and reducer.

--- part7/blogs-redux/src/index.js
+++ part7/blogs-redux/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import { Provider } from 'react-redux'
+import App from './App'
 import store from './store'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
