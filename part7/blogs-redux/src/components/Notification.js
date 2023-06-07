bac300f6632c8780ba27ee8d583a1b30ec26e7a3
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-06-07 10:41:13 +0200

    Fix bug in Notification component. Ex. 7.17 done.

--- part7/blogs-redux/src/components/Notification.js
+++ part7/blogs-redux/src/components/Notification.js
@@ -4,12 +4,13 @@ import { Snackbar, Alert } from '@mui/material'
 const Notification = () => {
     const message = useSelector((state) => state.notifications.message)
     const error = useSelector((state) => state.notifications.error)
+    console.log('Message = ', message)
 
     return (
         <Snackbar
-            open={message !== null}
+            open={message !== null && message !== ''}
             autoHideDuration={6000}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
         >
             <Alert severity={error ? 'error' : 'success'}>{message}</Alert>
         </Snackbar>
