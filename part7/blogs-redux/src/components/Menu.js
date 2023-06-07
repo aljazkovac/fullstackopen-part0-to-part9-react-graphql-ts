296c74ba4649ec349d90898cc2723ad2097cea95
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-06-07 09:13:32 +0200

    Change Menu to use AppBar.

--- part7/blogs-redux/src/components/Menu.js
+++ part7/blogs-redux/src/components/Menu.js
@@ -1,23 +1,28 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import React from 'react'
+import { AppBar, Toolbar, Button } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import LandingPage from './LandingPage'
 import UsersTable from './UsersTable'
 import User from './User'
 import Blog from './Blog'
 
 const Menu = ({ user }) => {
-    const padding = {
-        paddingRight: 5,
-    }
     return (
         <div>
-            <div>
-                <Link style={padding} to="/">
-                    blogs
-                </Link>
-                <Link style={padding} to="/users">
-                    users
-                </Link>
-            </div>
+            <AppBar
+                position="static"
+                sx={{ backgroundColor: '#f5f5f5', color: 'black' }}
+            >
+                <Toolbar>
+                    <Button color="inherit" component={Link} to="/">
+                        Blogs
+                    </Button>
+                    <Button color="inherit" component={Link} to="/users">
+                        Users
+                    </Button>
+                </Toolbar>
+            </AppBar>
             <Routes>
                 <Route path="/" element={<LandingPage user={user} />} />
                 <Route path="/users" element={<UsersTable />} />
