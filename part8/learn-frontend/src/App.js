bdf54f915c57170174c2927509989441b5b1fb6a
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-06-23 20:58:12 +0200

    Initialize App.js

--- part8/learn-frontend/src/App.js
+++ part8/learn-frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-export default App;
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
+client.query({ query }).then((response) => {
+  console.log(response.data);
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
