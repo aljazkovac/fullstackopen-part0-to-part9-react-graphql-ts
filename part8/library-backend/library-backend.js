73a07472b0c87a94c44f9008f6ca555c5d9db0ec
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-07-01 12:01:30 +0200

    Implement user login.

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -7,8 +7,11 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const Author = require("./models/author");
 const Book = require("./models/book");
+const Person = require("./models/person");
+const User = require("./models/user");
 require("dotenv").config();
 
+const jwt = require("jsonwebtoken");
 const MONGODB_URI = process.env.MONGODB_URI;
 
 console.log("connecting to", MONGODB_URI);
@@ -36,15 +39,26 @@ const typeDefs = `
     born: Int
     bookCount: Int
   }
-  type AuthorAndBook {
-    author: Author!
-    book: Book!
+  type Person {
+    name: String!
+    phone: Int
+    street: String
+    city: String
+  }
+  type User {
+    username: String!
+    friends: [Person!]!
+    id: ID!
+  }
+  type Token {
+    value: String!
   }
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
   type Mutation {
     addBook(
@@ -61,6 +75,13 @@ const typeDefs = `
       name: String!
       setBornTo: Int!
     ): Author
+    createUser(
+      username: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `;
 
@@ -109,8 +130,10 @@ const resolvers = {
       }
       return Book.find(filters);
     },
-
     allAuthors: async () => Author.find({}),
+    me: (root, args, context) => {
+      return context.currentUser;
+    },
   },
   Author: {
     bookCount: async (author) => {
@@ -177,6 +200,37 @@ const resolvers = {
       await saveDocument(existingAuthor);
       return existingAuthor;
     },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username });
+
+      return user.save().catch((error) => {
+        throw new GraphQLError("Creating the user failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+            error,
+          },
+        });
+      });
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== "secret") {
+        throw new GraphQLError("wrong credentials", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+    },
   },
 };
 
@@ -187,6 +241,27 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  /*
+  "During a GraphQL operation, you can share data throughout your server's resolvers and plugins 
+  by creating an object named contextValue. You can pass useful things through your contextValue that 
+  any resolver might need, like authentication scope, sources for fetching data, database connections, 
+  and custom fetch functions. If you're using dataloaders to batch requests across resolvers, you can also 
+  attach them to the shared contextValue."
+  https://www.apollographql.com/docs/apollo-server/data/context/
+  */
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwt.verify(
+        auth.substring(7),
+        process.env.JWT_SECRET
+      );
+      const currentUser = await User.findById(decodedToken.id).populate(
+        "friends"
+      );
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
