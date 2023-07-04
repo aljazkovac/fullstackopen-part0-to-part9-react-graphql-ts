8039c03b0b743843bec0a4ca0fff3ab5b5ccbb97
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-07-04 22:01:18 +0200

    Continue refactor. WIP.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,11 +1,20 @@
-const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
 require("dotenv").config();
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require("@apollo/server/plugin/drainHttpServer");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
-typeDefs = require("./schema");
-resolvers = require("./resolvers");
+const User = require("./models/user");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
 console.log("connecting to", MONGODB_URI);
 
@@ -18,35 +27,40 @@ mongoose
     console.log("error connecting to MongoDB:", error.message);
   });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+// setup is now within a function
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  /*
-  "During a GraphQL operation, you can share data throughout your server's resolvers and plugins 
-  by creating an object named contextValue. You can pass useful things through your contextValue that 
-  any resolver might need, like authentication scope, sources for fetching data, database connections, 
-  and custom fetch functions. If you're using dataloaders to batch requests across resolvers, you can also 
-  attach them to the shared contextValue."
-  https://www.apollographql.com/docs/apollo-server/data/context/
-  */
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null;
-    console.log("Auth in context server: ", auth);
-    if (auth && auth.toLowerCase().startsWith("bearer ")) {
-      const decodedToken = jwt.verify(
-        auth.substring(7),
-        process.env.JWT_SECRET
-      );
-      console.log("Decoded token: ", decodedToken);
-      const currentUser = await User.findById(decodedToken.id);
-      console.log("Current user: ", currentUser);
-      return { currentUser };
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  app.use(
+    "/",
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null;
+        if (auth && auth.toLowerCase().startsWith("bearer ")) {
+          const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+          const currentUser = await User.findById(decodedToken.id);
+          console.log("currentUser", currentUser);
+          return { currentUser };
+        }
+      },
+    })
+  );
+
+  const PORT = 4000;
+
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}`)
+  );
+};
+
+start();
